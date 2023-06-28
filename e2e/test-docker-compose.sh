840c8cda63cf46cdcb41665e7b07706d9f1c1b17
giorgio <giorgio.baldaccini@moviri.com>
2023-06-28 12:45:57 +0200

    e2e docker-compose only on develop/master/MRs

--- e2e/test-docker-compose.sh
+++ e2e/test-docker-compose.sh
@@ -7,10 +7,11 @@ curr_password=$(docker-compose logs management-container | grep Password | cut -
 container_id=$(docker ps | grep management-container | cut -d ' ' -f 1)
 docker cp test-remote-ssh.sh ${container_id}:/tmp/
 docker exec $container_id /tmp/test-remote-ssh.sh "$curr_password"
-if [ $? -eq 0 ]; then
+res=$?
+docker-compose down
+if [ $res -eq 0 ]; then
 	echo "Test PASSED"
 else
 	echo "Test FAILED"
 	exit 1
 fi
-docker-compose down
