643addfb6bc636f2891800880efdf8d61cd27403
Giovanni Paolo Gibilisco <giovanni.gibilisco@akamas.io>
2023-07-26 14:57:12 +0200

    add logging

--- e2e/test-docker-compose.sh
+++ e2e/test-docker-compose.sh
@@ -2,7 +2,9 @@
 
 docker-compose pull
 docker-compose up -d
+docker ps
 sleep 10
+docker ps
 curr_password=$(docker-compose logs management-container | grep Password | cut -d ':' -f 2 | sed 's/ //')
 container_id=$(docker ps | grep management-container | cut -d ' ' -f 1)
 docker cp test-remote-ssh-docker.sh ${container_id}:/tmp/
