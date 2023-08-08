1ec87be018b49f81be621924c273d4699c56b056
Matteo Stringher <matteo.stringher@moviri.com>
2023-08-08 18:55:35 +0200

    fix pipeline

--- e2e/test-docker-compose.sh
+++ e2e/test-docker-compose.sh
@@ -4,7 +4,7 @@ docker-compose pull
 docker-compose up -d
 sleep 10
 docker ps
-curr_password=$(docker-compose logs management-container | grep Password | cut -d ':' -f 2 | sed 's/ //')
+curr_password=$(docker-compose logs management-container | grep -i password | grep -o "'[^']*'" | sed -n '2p' | tr -d "'\n")
 container_id=$(docker ps | grep management-container | cut -d ' ' -f 1)
 docker cp test-remote-ssh-docker.sh ${container_id}:/tmp/
 docker exec $container_id /tmp/test-remote-ssh-docker.sh "$curr_password"
