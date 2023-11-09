bc38bbc865b7553138aad5e28286bccea5c4892a
Luca Cavazzana <luca.cavazzana@akamas.io>
2023-11-09 16:55:33 +0100

    e2e tests

--- e2e/test-kubernetes.sh
+++ e2e/test-kubernetes.sh
@@ -6,7 +6,7 @@
 aws eks --region 'us-east-2' update-kubeconfig --name "$1"
 kubectl config set-context --current --namespace=$2
 management_pod_name=$(kubectl get pods | grep management | head -1 | cut -d ' ' -f 1)
-curr_password=$(kubectl logs ${management_pod_name} | grep -i password | grep -o "'[^']*'" | sed -n '2p' | tr -d "'\n")
+curr_password=$(kubectl logs ${management_pod_name} | grep -i 'You can ssh into this container' | grep -o "'[^']*'" | sed -n '2p' | tr -d "'\n")
 kubectl cp test-remote-ssh-kube.sh ${2}/${management_pod_name}:/tmp/
 kubectl exec ${management_pod_name} -- /bin/bash -c "/tmp/test-remote-ssh-kube.sh $curr_password ${management_pod_name}"
 res=$?
