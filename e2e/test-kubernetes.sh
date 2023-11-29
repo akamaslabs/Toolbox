d41f6b86fb5489933109c3e6e6f98372b7621a90
giorgio <giorgio.baldaccini@moviri.com>
2023-11-29 17:35:46 +0100

    Fixed error in kube e2e test

--- e2e/test-kubernetes.sh
+++ e2e/test-kubernetes.sh
@@ -6,7 +6,7 @@
 aws eks --region 'us-east-2' update-kubeconfig --name "$1"
 kubectl config set-context --current --namespace=$2
 management_pod_name=$(kubectl get pods | grep management | head -1 | cut -d ' ' -f 1)
-curr_password=$(kubectl logs ${management_pod_name} | grep -i 'You can ssh into this container' | grep -o "'[^']*'" | sed -n '2p' | tr -d "'\n")
+curr_password=$(kubectl logs ${management_pod_name} 2>/dev/null | grep -i 'You can ssh into this container' | grep -o "'[^']*'" | sed -n '2p' | tr -d "'\n")
 kubectl cp test-remote-ssh.sh ${2}/${management_pod_name}:/tmp/
 kubectl exec ${management_pod_name} -- /bin/bash -c "/tmp/test-remote-ssh.sh $curr_password management-pod"
 res=$?
